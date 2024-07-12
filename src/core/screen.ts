2b870e54d5d24eca10e379e8bb7c5eb9fe9b0f97
andraswebcode <andras.tovishati@gmail.com>
2024-07-12 15:43:33 +0200

    Set up webgl program

---
+++ src/core/screen.ts
@@ -0,0 +1,89 @@
+class Screen {
+	public canvas: HTMLCanvasElement;
+	public context: WebGLRenderingContext | null;
+	public program: WebGLProgram | null;
+
+	public constructor(canvas: HTMLCanvasElement) {
+		this.canvas = canvas;
+		this.context = canvas.getContext('webgl');
+		this.program = this._createShaderProgram();
+	}
+
+	public update() {
+		const gl = this.context;
+		const { width, height } = this.canvas;
+
+		if (!gl) {
+			return this;
+		}
+
+		gl.viewport(0, 0, width, height);
+		gl.clearColor(0, 0, 0, 1);
+		gl.clear(gl.COLOR_BUFFER_BIT);
+
+		gl.useProgram(this.program);
+
+		return this;
+	}
+
+	private _createShaderProgram(): WebGLProgram | null {
+		const gl = this.context;
+
+		if (!gl) {
+			return null;
+		}
+
+		const program = gl.createProgram();
+		const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+		const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+		if (!program || !vertexShader || !fragmentShader) {
+			return null;
+		}
+
+		gl.shaderSource(vertexShader, this._getVertexSource());
+		gl.shaderSource(fragmentShader, this._getFragmentSource());
+
+		gl.compileShader(vertexShader);
+		gl.compileShader(fragmentShader);
+
+		if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+			console.log(gl.getShaderInfoLog(vertexShader));
+		}
+		if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+			console.log(gl.getShaderInfoLog(fragmentShader));
+		}
+
+		gl.attachShader(program, vertexShader);
+		gl.attachShader(program, fragmentShader);
+
+		gl.linkProgram(program);
+
+		if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+			console.log(gl.getProgramInfoLog(program));
+		}
+
+		this.update();
+
+		return program;
+	}
+
+	private _getVertexSource() {
+		return `
+			attribute vec4 aPos;
+			void main(){
+				gl_Position = aPos;
+			}
+		`;
+	}
+
+	private _getFragmentSource() {
+		return `
+			void main(){
+				gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+			}
+		`;
+	}
+}
+
+export { Screen };
